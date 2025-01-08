42b0381fff6376441c8a013ec64803ef1e62a1ec
Naufal Abrori <nopal.abrori@gmail.com>
2025-01-08 10:05:12 +0700

    fix: cors error

--- main.go
+++ main.go
@@ -7,21 +7,33 @@ import (
 	"time"
 
 	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
 )
 
 func main() {
 	config.ConnectDB()
 	database.Migrate()
-	r := routes.SetupRoutes()
+
+	r := gin.Default()
 
 	r.Use(cors.New(cors.Config{
-		AllowOrigins:     []string{"http://localhost:3000"},        // Origin frontend Anda
-		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"}, // Metode HTTP yang diizinkan
+		AllowOrigins:     []string{"http://localhost:3000"},
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
 		AllowHeaders:     []string{"Content-Type", "Authorization"},
 		ExposeHeaders:    []string{"Content-Length"},
 		AllowCredentials: true,
-		MaxAge:           12 * time.Hour, // Cache preflight request selama 12 jam
+		MaxAge:           12 * time.Hour,
 	}))
+
+	r.OPTIONS("/*path", func(c *gin.Context) {
+		c.Header("Access-Control-Allow-Origin", "http://localhost:3000")
+		c.Header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE")
+		c.Header("Access-Control-Allow-Headers", "Content-Type, Authorization")
+		c.Header("Access-Control-Allow-Credentials", "true")
+		c.Status(200)
+	})
+
+	routes.SetupRoutes(r)
 
 	r.Run(":8080")
 }
--- routes/routes.go
+++ routes/routes.go
@@ -7,8 +7,8 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func SetupRoutes() *gin.Engine {
-	r := gin.Default()
+func SetupRoutes(r *gin.Engine) *gin.Engine {
+	// r := gin.Default()
 
 	publicGroup := r.Group("/")
 	{
